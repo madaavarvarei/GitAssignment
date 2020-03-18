d44b30c02308b6aed0bf1991df7f1d0954c2b8dd
Madalina <Madalina@192.168.1.6>
2020-03-18 09:03:45 +0200

    Commit 2
    #A fost adaugata clasa Restaurant
    #Clasa Restaurant are doua atribute de tip String: denumire, adresa
    #Clasa Restaurant are un constructor cu toti parametri

---
+++ GitAssignment/src/ro/ase/csie/cts/g1074/tema2/Restaurant.java
@@ -0,0 +1,14 @@
+package ro.ase.csie.cts.g1074.tema2;
+
+public class Restaurant {
+
+	String denumire;
+	String adresa;
+	
+	public Restaurant(String denumire, String adresa) {
+		super();
+		this.denumire = denumire;
+		this.adresa = adresa;
+	}
+}
+
