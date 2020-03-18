9c2d9904c8995fc299e50cf6ebd1dabbb1bb9e96
Madalina <Madalina@192.168.1.6>
2020-03-18 09:01:20 +0200

    Commit initial

---
+++ GitAssignment/src/ro/ase/csie/cts/g1074/tema2/TestGit.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.g1074.tema2;
+
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Git ! Denumirea proiectului de licență este "
+				+ "Dezvoltarea unei aplicații în sectorul HoReCa");
+	}
+}
+
